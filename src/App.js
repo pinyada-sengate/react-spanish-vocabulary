d18b56880bccaec55c859176f3f6b41b39320f47
Pinyada Sengate <pinyada.sengate@gmail.com>
2025-02-01 22:25:21 +0100

    implement react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-function App() {
-  return <h1>Hello React</h1>;
-}
+import React from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+
+import Users from "./user/pages/Users";
+import AddCategory from "./category/pages/AddCategory";
+import { Switch } from "react-router-dom/cjs/react-router-dom.min";
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/category/add" exact>
+          <AddCategory />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/category/pages/AddCategory.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddCategory = () => {
+  return <h2>Add Category!</h2>;
+};
+
+export default AddCategory;
