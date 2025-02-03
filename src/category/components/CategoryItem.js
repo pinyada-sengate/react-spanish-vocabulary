cec009efc543f327843741fe65f7dc20cd69e0e2
Pinyada Sengate <pinyada.sengate@gmail.com>
2025-02-03 16:37:03 +0100

    add category item component

---
+++ src/category/components/CategoryItem.js
@@ -0,0 +1,95 @@
+import React, { useState, useContext } from "react";
+
+import "./CategoryItem.css";
+
+import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
+import Modal from "../../shared/components/UIElements/Modal";
+import { AuthContext } from "../../shared/context/auth-context";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+
+const CategoryItem = (props) => {
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const auth = useContext(AuthContext);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+
+  const showDeleteWarningHandler = () => {
+    setShowConfirmModal(true);
+  };
+
+  const cancelDeleteHandler = () => {
+    setShowConfirmModal(false);
+  };
+
+  const confirmDeleteHandler = async () => {
+    setShowConfirmModal(false);
+
+    try {
+      await sendRequest(
+        `${process.env.REACT_APP_BACKEND_URL}/category/delete/${props.id}`,
+        "DELETE",
+        null,
+        {
+          Authorization: "Bearer " + auth.token,
+        }
+      );
+
+      props.onDelete(props.id);
+    } catch (err) {}
+  };
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+
+      <Modal
+        show={showConfirmModal}
+        onCancel={cancelDeleteHandler}
+        header="Are you sure"
+        footerClass="category_item__modal-actions"
+        footer={
+          <React.Fragment>
+            <Button inverse onClick={cancelDeleteHandler}>
+              CANCEL
+            </Button>
+            <Button danger onClick={confirmDeleteHandler}>
+              DELETE
+            </Button>
+          </React.Fragment>
+        }
+      >
+        <p>Do you want to proceed to delete this category?</p>
+      </Modal>
+
+      <li className="category_item">
+        <Card className="category_item__content">
+          {isLoading && <LoadingSpinner asOverlay />}
+          <div className="category_item__image">
+            <img
+              src={`${process.env.REACT_APP_ASSET_URL}/${props.image}`}
+              alt={props.title}
+            />
+          </div>
+          <div className="category_item__info">
+            <h2>{props.title}</h2>
+          </div>
+          <div className="category_item__actions">
+            {auth.userId === props.creatorId && (
+              <Button to={`/category/${props.id}`}>EDIT</Button>
+            )}
+
+            {auth.userId === props.creatorId && (
+              <Button danger onClick={showDeleteWarningHandler}>
+                DELETE
+              </Button>
+            )}
+          </div>
+        </Card>
+      </li>
+    </React.Fragment>
+  );
+};
+
+export default CategoryItem;
