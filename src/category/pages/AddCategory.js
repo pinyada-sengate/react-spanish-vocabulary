765f231fc2ab8656e628c234553435e8b5c7d889
Pinyada Sengate <pinyada.sengate@gmail.com>
2025-02-03 12:13:30 +0100

    add category page

--- src/category/pages/AddCategory.js
+++ src/category/pages/AddCategory.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
 
+import Input from "../../shared/components/FormElements/Input";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+import Button from "../../shared/components/FormElements/Button";
 import { useForm } from "../../shared/hooks/form-hook";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import { AuthContext } from "../../shared/context/auth-context";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import ImageUpload from "../../shared/components/FormElements/ImageUpload";
 
 const AddCategory = () => {
+  const auth = useContext(AuthContext);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [formState, inputHandler] = useForm(
     {
       title: {
@@ -17,10 +31,33 @@ const AddCategory = () => {
     false
   );
 
+  const history = useHistory();
+
+  const categorySubmitHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      const formData = new FormData();
+      formData.append("title", formState.inputs.title.value);
+      formData.append("image", formState.inputs.image.value);
+      await sendRequest(
+        `${process.env.REACT_APP_BACKEND_URL}/category`,
+        "POST",
+        formData,
+        {
+          Authorization: "Barrer " + auth.token,
+        }
+      );
+
+      //redirect user to home
+      history.push("/");
+    } catch (err) {}
+  };
+
   return (
     <React.Fragment>
       <ErrorModal error={error} onClear={clearError} />
-      <form className="place-form" onSubmit={placeSubmitHandler}>
+      <form className="place-form" onSubmit={categorySubmitHandler}>
         {isLoading && <LoadingSpinner asOverlay />}
         <Input
           id="title"
