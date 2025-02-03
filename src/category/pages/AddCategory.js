dd223362b10bdb491c9cae78a0bea3014b13a335
Pinyada Sengate <pinyada.sengate@gmail.com>
2025-02-03 12:04:24 +0100

    change direction of hooks folder

--- src/category/pages/AddCategory.js
+++ src/category/pages/AddCategory.js
@@ -1,7 +1,48 @@
 import React from "react";
 
+import { useForm } from "../../shared/hooks/form-hook";
+
 const AddCategory = () => {
-  return <h2>Add Category!</h2>;
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      image: {
+        value: null,
+        isValid: false,
+      },
+    },
+    false
+  );
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      <form className="place-form" onSubmit={placeSubmitHandler}>
+        {isLoading && <LoadingSpinner asOverlay />}
+        <Input
+          id="title"
+          element="input"
+          type="text"
+          label="Title"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid title."
+          onInput={inputHandler}
+        />
+        <ImageUpload
+          center
+          id="image"
+          onInput={inputHandler}
+          errorText="Please provide an image."
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          ADD CATEGORY
+        </Button>
+      </form>
+    </React.Fragment>
+  );
 };
 
 export default AddCategory;
