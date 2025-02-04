00812a36e49744825a3e2ec53d5fbf7fc358785a
Pinyada Sengate <pinyada.sengate@gmail.com>
2025-02-04 18:02:28 +0100

    remove console log in auth

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -67,8 +67,6 @@ const Auth = () => {
   const authSubmitHandler = async (event) => {
     event.preventDefault();
 
-    console.log(formState.inputs);
-
     if (isLoginMode) {
       try {
         const responseData = await sendRequest(
