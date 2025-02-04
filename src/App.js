5a80ba3c92809795e6500cfc272007b3818e0308
Pinyada Sengate <pinyada.sengate@gmail.com>
2025-02-04 11:08:35 +0100

    add auth route

--- src/App.js
+++ src/App.js
@@ -9,23 +9,56 @@ import {
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import AddCategory from "./category/pages/AddCategory";
 import Categories from "./category/pages/Categories";
+import Auth from "./user/pages/Auth";
+
+import { AuthContext } from "./shared/context/auth-context";
+import { useAuth } from "./shared/hooks/auth-hook";
 
 const App = () => {
+  const { token, login, logout, userId } = useAuth();
+
+  let routes;
+  if (token) {
+    routes = (
+      <Switch>
+        <Route path="/category/getCategories" exact>
+          <Categories />
+        </Route>
+        <Route path="/category/add" exact>
+          <AddCategory />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/category/getCategories" exact>
+          <Categories />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Redirect to="/auth" />
+      </Switch>
+    );
+  }
+
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Switch>
-          <Route path="/category/getCategories" exact>
-            <Categories />
-          </Route>
-          <Route path="/category/add" exact>
-            <AddCategory />
-          </Route>
-          <Redirect to="/" />
-        </Switch>
-      </main>
-    </Router>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: !!token,
+        token,
+        userId,
+        login,
+        logout,
+      }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>{routes}</main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
